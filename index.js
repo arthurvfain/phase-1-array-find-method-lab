b6623bfc7e240d798bb5f7907932fe59f55e868c
Arthur V Fain <arthurvfain@gmail.com>
2021-05-20 10:10:27 -0400

    commit tree a04874c4823718307567b07529eeff2f1133f6ed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+// function superbowlWin(arr)
+// {
+//     if (arr.find(year => year.result === `W`) === undefined)
+//     {
+//         return undefined;
+//     }
+//     else
+//     {
+//         return (arr.find(year => year.result === `W`)).year;
 
+//     }
+// }
+
+function superbowlWin(arr)
+{
+    if (arr.find(year => year.result === `W`) !== undefined)
+    {
+        return (arr.find(year => year.result === `W`)).year;
+    }
+    else
+    {
+        return undefined;
+
+    }
+}
